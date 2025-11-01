8ad89cc7b926893f62a1c88ab5663f5f138051e3
jul109 <115905294+jul109@users.noreply.github.com>
2025-11-01 18:36:34 -0500

    feat login: get token service

--- app/(general)/users/page.tsx
+++ app/(general)/users/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+import { useEffect, useState } from "react";
+import { getUsers } from "@/app/actions/users/users";
 export default function Home() {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const users = await getUsers();
+      setUsers(users);
+      console.log(users)
+    })()
+
+  }, [])
+
   return (
-   <div>
-    Usuarios
-   </div> 
+    <div>
+     
+      Usuarios
+    </div>
   );
 }
 
